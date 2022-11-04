ad2e48aeaed6d146ead18bd26ee758e73dc7b7b4
Gringooh <116928287+Gringooh@users.noreply.github.com>
2022-11-04 15:09:12 -0300

    save project

---
+++ coversores/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  coversores
+//
+//  Created by Gringo on 27/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    @IBOutlet weak var lbUnit: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showNext(_ sender: UIButton) {
+        switch lbUnit.text! {
+            
+        }
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+
+}
+
